884450188d29cc18f46cf0518f67f2648fc4fce8
Leeminsu67 <alstn67@gmail.com>
2022-11-27 03:10:43 +0900

    study javascript

--- app.js
+++ app.js
@@ -1,37 +1,6 @@
-const player = {
-	name: "nico",
-	points: 10,
-	fat: true,
-}
-console.log(player);
-player.lastName = "potato";
-player.points = 15;
-console.log(player);
+const title = document.getElementById("title");
 
+title.innerText = "Got you!";
 
-const calculator = {
-	plus: (a, b) => {
-		console.log(a + b);
-		return a + b
-	},
-	minus: (a, b) => {
-		console.log(a - b);
-		return a - b;
-	},
-	divide: (a, b) => {
-		console.log(a / b);
-		return a / b;
-	},
-	max: (a, b) => {
-		console.log(a * b);
-		return a * b;
-	},
-	power: (a, b) => {
-		console.log(a ** b);
-		return a ** b;
-	},
-}
-
-const value = calculator.plus(2, 3);
-console.log(value);
-console.log()
+console.log(title.id);
+console.log(title.className);
