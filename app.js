980a5b2f3c59e966382735c5e789f4b3560f1ce9
Leeminsu67 <alstn67@gmail.com>
2022-11-26 23:50:28 +0900

    start study

---
+++ app.js
@@ -0,0 +1,36 @@
+const player = {
+	name: "nico",
+	points: 10,
+	fat: true,
+}
+console.log(player);
+player.lastName = "potato";
+player.points = 15;
+console.log(player);
+
+
+const calculator = {
+	plus: (a, b) => {
+		console.log(a + b);
+		return a + b
+	},
+	minus: (a, b) => {
+		console.log(a - b);
+		return a - b;
+	},
+	divide: (a, b) => {
+		console.log(a / b);
+		return a / b;
+	},
+	max: (a, b) => {
+		console.log(a * b);
+		return a * b;
+	},
+	power: (a, b) => {
+		console.log(a ** b);
+		return a ** b;
+	},
+}
+
+const value = calculator.plus(2, 3);
+console.log(value);
